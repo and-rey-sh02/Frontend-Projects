0366a5c0ad4f30007f089bd60ec77c4e8cae552b
and-rey-sh02 <fannytv02@gmail.com>
2025-08-17 13:49:42 +0300

    Добавлены новые файлы и исправлены правки

--- tvmaze-finder/src/index.js
+++ tvmaze-finder/src/index.js
@@ -8,7 +8,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Frontend-Projects/tvmaze-finder">
       <FavoritesProvider>
         <App />
       </FavoritesProvider>
